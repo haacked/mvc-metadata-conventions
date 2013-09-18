185c6114dbb9cd06babfc533caacd2a871e95184
Haacked <haacked@gmail.com>
2013-09-18 09:59:14 -0700

    Add a build script to create NuGet packages

--- ModelMetadataExtensions/Properties/AssemblyInfo.cs
+++ ModelMetadataExtensions/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ModelMetadataExtensions")]
 [assembly: AssemblyDescription("Extension to Model Metadata that provide convention based resource lookup")]
 [assembly: AssemblyCompany("Phil Haack")]
 [assembly: AssemblyProduct("ModelMetadataExtensions")]
-[assembly: AssemblyCopyright("Copyright © Phil Haack 2010")]
+[assembly: AssemblyCopyright("Copyright © Phil Haack 2013")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -19,15 +16,13 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7aa5644d-b24f-4937-a24d-dee7b5b99604")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion(AssemblyConstants.AssemblyVersion)]
+[assembly: AssemblyFileVersion(AssemblyConstants.AssemblyVersion)]
+[assembly: AssemblyInformationalVersion(AssemblyConstants.PackageVersion)]
+
+internal static class AssemblyConstants
+{
+    internal const string PackageVersion = "0.1.2";
+    internal const string PrereleaseVersion = ""; // Until we ship 1.0, this isn't necessary.
+    internal const string AssemblyVersion = PackageVersion + ".0";
+}
